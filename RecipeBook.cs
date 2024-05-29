6bf7c7135a4110b67c68396747b3cdb87ea7f831
ShivarTuplah <ST10256115@vcconnect.edu.za>
2024-05-29 12:53:26 +0200

    Added in recipebook class

---
+++ RecipeBook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApp
+{
+    public class RecipeBook
+    {
+        private List<Recipe> recipes;
+
+        public RecipeBook()
+        {
+            recipes = new List<Recipe>();
+        }
+
+        public void AddRecipe(Recipe recipe)
+        {
+            recipe.OnCaloriesExceeded += DisplayCalorieWarning;
+            recipes.Add(recipe);
+        }
+
+        public List<Recipe> GetAllRecipes()
+        {
+            return recipes;
+        }
+
+        private void DisplayCalorieWarning(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            ResetConsoleColor();
+        }
+
+        private void ResetConsoleColor()
+        {
+            Console.ResetColor();
+        }
+    }
+}
