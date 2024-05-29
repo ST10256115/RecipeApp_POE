a246f3a8bc78dfe1231e2fced6bafc4a6c3e9579
ShivarTuplah <ST10256115@vcconnect.edu.za>
2024-05-29 12:54:32 +0200

    Ingredients class updated

--- Ingredient.cs
+++ Ingredient.cs
@@ -5,17 +5,22 @@
         public string Name { get; set; }
         public double Quantity { get; set; }
         public string Unit { get; set; }
+        public double Calories { get; set; }
+        public string FoodGroup { get; set; }
 
-        public Ingredient(string name, double quantity, string unit)
+        public Ingredient(string name, double quantity, string unit, double calories, string foodGroup)
         {
             Name = name;
             Quantity = quantity;
             Unit = unit;
+            Calories = calories;
+            FoodGroup = foodGroup;
         }
 
         public override string ToString()
         {
-            return $"{Quantity} {Unit} of {Name}";
+            return $"{Quantity} {Unit} of {Name} ({Calories} calories, {FoodGroup})";
         }
     }
 }
+
